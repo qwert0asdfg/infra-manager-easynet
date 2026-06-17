09561bd608965d099e9644214c18ab5e85d22ddd
panlingnan <panlingnan@bytedance.com>
2026-06-17 15:44:26 +0800

    xx

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ variable "vpc_cidr_block" {
 variable "subnet_name" {
   description = "Name of the subnet"
   type        = string
-  default     = "subnet-demo"
+  default     = "subnet-demo-update-code"
 }
 
 variable "subnet_description" {
